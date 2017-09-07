db4aa4406bacb410371c1c2ec745311ebb13620d
danebou <danebouchie@knights.ucf.edu>
2017-09-07 18:22:04 +0000

    Renamed Vector3f again

---
+++ head_tracking/utility/Vector3f.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR3F_H
+#define VECTOR3F_H
+
+struct Vector3f
+{
+    float x, y, z;
+
+    Vector3f();
+    Vector3f(float x, float y, float z);
+
+	Vector3f operator+(const Vector3f & other) const;
+	//float operator*(const Vector3f & other) const;
+	//Vector3f operator*(const float & other) const;
+	bool operator==(const Vector3f& rhs) const;
+	bool operator!=(const Vector3f& rhs) const;
+};
+
+#endif /* VECTOR3F_H */
